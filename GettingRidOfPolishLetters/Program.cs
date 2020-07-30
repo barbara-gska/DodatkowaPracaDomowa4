89fa7e9c12399b1db6fc02143db52b724af52e8c
Barbara Gska <pacuszka.barbara@gmail.com>
2020-07-30 21:29:36 +0200

    Adding project files

---
+++ GettingRidOfPolishLetters/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingRidOfPolishLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To Twoja szansa na pozbycie się udziwnionych polskich liter z danego wyrażenia");
+            
+            do
+            {
+                Console.WriteLine("Wpisz tekst, który chcesz zmienić: ");
+                var phrase = Console.ReadLine();
+
+                Console.WriteLine("Poprawiony tekst to: ");
+                Console.WriteLine(ReplacingPolishLetters.MakeItNormal(phrase));
+
+                Console.WriteLine("Kolejny tekst do zmiany [t/n]?");
+                var gameOn = Console.ReadLine().ToLower();
+                if (gameOn == "t")
+                    continue;
+                else if (gameOn == "n")
+                {
+                    Console.WriteLine("Zakończono żmudną pracę polegającą na zamianie liter polskich na normalne.");
+                    Console.WriteLine("See you again next time");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Nie wiem co chciałbys zrobić. W związku z tym kończę pracę.");
+                    break;
+                }
+
+            } while (true);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GettingRidOfPolishLetters/ReplacingPolishLetters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingRidOfPolishLetters
+{
+    public static class ReplacingPolishLetters
+    {
+        public static string MakeItNormal(string text)
+        {
+            string result;
+
+            result = text.Replace('ę', 'e').Replace('ó', 'o').Replace('ą', 'a').Replace('ś', 's')
+                            .Replace('ł', 'l').Replace('ż', 'z').Replace('ź', 'z').Replace('ć', 'c').Replace('ń', 'n');
+
+            return result;
+        }
+    }
+}
